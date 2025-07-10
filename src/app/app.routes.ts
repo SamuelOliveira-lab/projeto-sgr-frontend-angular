78fa9ec015e9298dbea8d3916b6b57fc2a174e55
Nathan Balmant <nathanbalmantdev@gmail.com>
2025-07-10 17:04:16 -0300

    Baixando angular materia e criando uma navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,21 @@ import { LoginComponent } from './pages/login/login.component';
 import { MoradorCadastroComponent } from './pages/morador-cadastro/morador-cadastro.component';
 import { SaldoComponent } from './pages/saldo/saldo.component';
 import { ContaComponent } from './pages/conta/conta.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 export const routes: Routes = [
       {
     path: 'login',
     component: LoginComponent
+  },
+    {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: 'morador', component: MoradorCadastroComponent },
+      { path: 'conta', component: ContaComponent },
+      { path: 'saldo', component: SaldoComponent }
+    ]
   },
   {
     path: '',
---
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    RouterModule
+  ],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.css'
+})
+export class MainLayoutComponent {
+
+}
