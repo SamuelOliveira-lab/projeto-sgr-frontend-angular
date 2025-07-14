b742bb6701d207452b61ba8de246378762d1d42b
Samuel Oliveira <samuferraz12052004@gmail.com>
2025-07-14 15:40:52 -0300

    Integração com a API para realizar o login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,28 +1,39 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,private http: HttpClient,private router: Router) {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      login: ['', [Validators.required]],
+      senha: ['', [Validators.required, Validators.minLength(2)]]
     });
   }
 
   onSubmit() {
     if (this.loginForm.valid) {
-      console.log(this.loginForm.value);
-      // Aqui você pode chamar um AuthService para autenticação real
+      const loginData = this.loginForm.value;
+      this.http.post('http://localhost:8080/moradores/auth/login', loginData, { responseType: 'text' })
+      .subscribe({
+        next: res => {
+          console.log('Login bem-sucedido:', res);
+          this.router.navigate(['/conta']); // Redirecionar após login
+        },
+        error: err => {
+          alert('Erro no login: ' + err.error);
+        }
+      });
     } else {
       this.loginForm.markAllAsTouched();
     }
